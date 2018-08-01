eb3ce339a7f81b9aaf1f947e8ad58ebebdf120f4
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-01 21:40:27 +0000

    Metro M4 Express no longer directly supported. 	modified:   gpio_local.c Project has become too specific to Feather M4 Express at the moment.

--- gpio_local.c
+++ gpio_local.c
@@ -9,20 +9,28 @@
 
 void init_act_LED(void) {
     gpio_set_pin_direction(PA23, GPIO_DIRECTION_OUT);
-    gpio_set_pin_direction(PA16, GPIO_DIRECTION_OUT);
+
+    // No longer supporting Metro M4 Express directly.
+
+    // gpio_set_pin_direction(PA16, GPIO_DIRECTION_OUT);
 }
+
+/*
 
 void raise_D13_metro(void) { // Metro M4 Express
     gpio_set_pin_level(PA16, true);
 }
+*/
 
 void raise_D13_feather(void) { // Feather M4 Express
     gpio_set_pin_level(PA23, true);
 }
 
+/*
 void lower_D13_metro(void) {
     gpio_set_pin_level(PA16, false);
 }
+*/
 
 void lower_D13_feather(void) {
     gpio_set_pin_level(PA23, false);
