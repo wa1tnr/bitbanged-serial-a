46788fa1f6b14827fd589e3f15344c8e60d2f5f8
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-08 10:11:34 +0000

    nice. untested - in a hurry bye 	modified:   main.c

--- main.c
+++ main.c
@@ -17,43 +17,7 @@ int main(void)
 */
 
 void pins_setup(void) {
-    // serial pins
-    //    input
-/*
-    PORT->Group[1].DIRCLR.reg = (uint32_t)(1 << 17); // rx is input // PB17
-    //    output
-    PORT->Group[1].DIRSET.reg = (uint32_t)(1 << 16); // tx output   // PB16
-
-    // gpio outputs
-    PORT->Group[0].DIRSET.reg |= (uint32_t)(1 << 18); // PA18 //  1  6 pinmode  // D6
-    PORT->Group[0].DIRSET.reg |= (uint32_t)(1 << 23); // PA23 //  1 13 pinmode  // D13
-*/
     PORT->Group[0].DIRSET.reg  = (uint32_t)(1 << 21); // PA21 //  1 11 pinmode  // D11
-/*
-
-    // gpio raise pin to 3.3v
-    PORT->Group[0].OUTSET.reg |= (uint32_t)(1 << 18); // PA18 //  1  6 pinwrite // D6
-    PORT->Group[0].OUTSET.reg |= (uint32_t)(1 << 21); // PA21 //  1 11 pinwrite // D11
-    PORT->Group[0].OUTSET.reg |= (uint32_t)(1 << 23); // PA23 //  1 13 pinwrite // D13
-
-    // gpio lower pin to Ground
-    PORT->Group[0].OUTCLR.reg =  (uint32_t)(1 << 18); // PA18 //  0  6 pinwrite // D6
-    PORT->Group[0].OUTCLR.reg |= (uint32_t)(1 << 21); // PA21 //  0 11 pinwrite // D11
-    PORT->Group[0].OUTCLR.reg =  (uint32_t)(1 << 23); // PA23 //  0 13 pinwrite // D13
-
-#undef PIN_D11_INPUT
-#ifdef PIN_D11_INPUT
-    // gpio input pins
-    PORT->Group[0].DIRCLR.reg  = (uint32_t)(1 << 21); // PA21 //  0 11 pinmode  // D11
-#endif // #ifdef PIN_D11_INPUT
-
-    // gpio output pin toggles
-    PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 18); // PA18 //  D6 toggle
-    PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 21); // PA21 // D11 toggle
-    PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 23); // PA23 // D13 toggle
-    // Group[1] means PORTB rather than PORTA (which is Group[0])
-    PORT->Group[1].OUTTGL.reg = (uint32_t)(1 <<  3); // PB03 //  D8 toggle  // NEOPIX
-*/
 }
 
 void uSec(void) {
@@ -65,7 +29,8 @@ void uSec(void) {
 void short_timer(void) { // human blinkie timescale
     uint32_t on_time  = 2140111222; // it's 2147 something ;)
     for(int j = 2; j>0; j--) {
-        for(on_time =       2140112; on_time > 0; on_time--) { // 21.4 million
+        // for(on_time =       2140112; on_time > 0; on_time--) { // 21.4 million
+        for(on_time =       214011; on_time > 0; on_time--) { // 21.4 million
             uSec();
         }
     }
@@ -122,26 +87,29 @@ void nmain(void) {
 }
 
 int main(void) {
-    // 660 kHz for a 1 uSec() iteration in the loop:
-    // atmel_start_init();
-    // system_init(); // driver_init.h
-    // SystemInit(); // recent work used this rather than atmel_start_init()
 
-    // test two: add just atmel_start_init():
-    // atmel_start_init(); // brings in system_init();
+/*
+    // Nice triple, 8 MHZ:
+    SystemCoreClockUpdate();
+    SystemInit();
+    pins_setup();
+*/
 
-    // test three: just SystemInit():
-
-    // 48 MHz:
+    SystemCoreClockUpdate(); // might pull in 48 MHz
     SystemInit(); // recent work used this rather than atmel_start_init()
     pins_setup();
 
+    // nmain(); // has fall-thru now.
+
+    // atmel_start_init();
+    // system_init(); // driver_init.h
+    // system_init(); // driver_init.c
     // clock_init();
     // smaller SysTick means ticks come more rapidly
     // SysTick_Config(4000000);
-    init_act_LED();
+    // init_act_LED();
 
-    nmain(); // has fall-thru now.
+    // nmain(); // has fall-thru now.
 
     while (1) {
         PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 21); // PA21 // D11 toggle
