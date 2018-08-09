e91189ba418ba56e28037428262db3b7c8d726b1
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-09 07:01:58 +0000

    The 8 MHZ RC clock code (measured; speculated on 'RC clock') 	modified:   main.c On branch clockey-amrt-aa-8_AUG-d-

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
-// Wed Aug  1 04:18:10 UTC 2018
+// Thu Aug  9 06:56:02 UTC 2018
 // main.c
+// 8 MHz (measured) RC clock (as a guess)
+// previous timestamp:
+// Wed Aug  1 04:18:10 UTC 2018
 #include "atmel_start.h"
 #include "gpio_local.h"
 #include "driver_init.h"
+#include "driver_examples.h"
+#include "clock_init.h"
 
 /*
 int main(void)
 {
 	// Initializes MCU, drivers and middleware //
 	atmel_start_init();
-
-	// Replace with your application code //
-	while (1) {
-	}
 }
 */
 
@@ -29,7 +30,6 @@ void uSec(void) {
 void short_timer(void) { // human blinkie timescale
     uint32_t on_time  = 2140111222; // it's 2147 something ;)
     for(int j = 2; j>0; j--) {
-        // for(on_time =       2140112; on_time > 0; on_time--) { // 21.4 million
         for(on_time =       214011; on_time > 0; on_time--) { // 21.4 million
             uSec();
         }
@@ -78,7 +78,6 @@ void long_long_timer(void) {
 
 void nmain(void) {
         raise_LED_pins();
-	// while (1) {
         for (int i=4; i>0; i--) { // blink a while then go into 3.5 MHz loop on D11
             flicker_LED();
             short_timer();
@@ -86,9 +85,18 @@ void nmain(void) {
         // fall-thru back to main();
 }
 
+/*
+void TC1_Handler(void){ // fires rarely, for counting overflows of time-ticker
+    // TC1->COUNT32.INTFLAG.bit.OVF = 1; // to clear it
+    PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 21); // PA21 // D11 toggle
+}
+*/
+
+/*
 void SysTick_Handler(void){
     PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 21); // PA21 // D11 toggle
 }
+*/
 
 int main(void) {
 
@@ -102,22 +110,33 @@ int main(void) {
     SystemCoreClockUpdate(); // might pull in 48 MHz
     SystemInit(); // recent work used this rather than atmel_start_init()
     pins_setup();
-    SysTick_Config(2); // 1.7 MHz on toggle D11
+
+    // TIMER_1_example();
+    // NVIC_EnableIRQ(TC1_IRQn);
+
+    // SysTick_Config(2); // 1.7 MHz on toggle D11
 
     // nmain(); // has fall-thru now.
 
     // atmel_start_init();
     // system_init(); // driver_init.h
+
+
+
+    // sytem_init() seemed a possible trouble spot and wasn't used earlier today (heh)
     // system_init(); // driver_init.c
-    // clock_init();
+    // NVIC_EnableIRQ(TC1_IRQn);
+
+
     // smaller SysTick means ticks come more rapidly
     // SysTick_Config(4000000);
     // init_act_LED();
 
     // nmain(); // has fall-thru now.
 
+    // clock_init(); // very late in the game
     while (1) {
-        // PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 21); // PA21 // D11 toggle
+        PORT->Group[0].OUTTGL.reg = (uint32_t)(1 << 21); // PA21 // D11 toggle
         // 3.5 MHz with no uSec loop
     }
 }
