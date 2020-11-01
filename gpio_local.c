4bf2a7a5755bb81de8e461e4fd7bc7f7afe991a0
wa1tnr <wa1tnr@users.noreply.github.com>
2020-11-01 16:18:45 +0000

    blinks D13 PA22

--- gpio_local.c
+++ gpio_local.c
@@ -1,13 +1,13 @@
 #include "driver_init.h" // gives atmel_start_pins.h for D13 and such
 
 void init_act_LED(void) {
-    gpio_set_pin_direction(PA23, GPIO_DIRECTION_OUT);
+    gpio_set_pin_direction(PA22, GPIO_DIRECTION_OUT);
 }
 
 void raise_D13_feather(void) { // Feather M4 Express
-    gpio_set_pin_level(PA23, true);
+    gpio_set_pin_level(PA22, true);
 }
 
 void lower_D13_feather(void) {
-    gpio_set_pin_level(PA23, false);
+    gpio_set_pin_level(PA22, false);
 }
